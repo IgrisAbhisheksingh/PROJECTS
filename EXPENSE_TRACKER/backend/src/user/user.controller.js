25797f6db961442d7458abcdda0082169b378057
Abhishek Singh <ABHISHEK8801108@GMAIL.COM>
2026-01-15 23:47:48 +0530

    working on login

--- EXPENSE_TRACKER/backend/src/user/user.controller.js
+++ EXPENSE_TRACKER/backend/src/user/user.controller.js
@@ -1,30 +1,52 @@
 import Usermodel from "./user.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-export const createUser =async (req,res) =>{
-    try{
-        const data =req.body;
-         const user =new Usermodel(data);
-         await user.save();
-         res.json(user);
-
+export const createUser = async (req, res) => {
+    try {
+        const data = req.body;
+        const user = new Usermodel(data);
+        await user.save();
+        res.json(user);
     }
-
-    catch(err)
-    {
-        res.status(500).json({message : err.message});
+    catch (err) {
+        res.status(500).json({ message: err.message });
     }
-}
+};
 
+const createToken = async (user) => {
+    const payload = {
+        id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+        role: user.role
+    };
+    const token = await jwt.sign(
+        payload,
+        process.env.AUTH_SECRET,
+        { expiresIn: "1d" }
+    );
+    return token;
+};
 
-export const login =async (req,res) =>{
-    try{
-        const data =req.body;
-        console.log(data);
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await Usermodel.findOne({ email });
 
+        if (!user)
+            return res.status(404).json({ message: "User not Found !" });
+
+        const isLoged = await bcrypt.compare(password, user.password);
+        if (!isLoged)
+            return res.status(401).json({ message: "Incorrect password!" });
+
+        const token = await createToken(user);
+
+        // ✅ missing response (main error)
+        res.json({ message: "Login successful", token });
     }
-
-    catch(err)
-    {
-        res.status(500).json({messaage : err.message});
+    catch (err) {
+        res.status(500).json({ message: err.message });
     }
-}
+};
