c42f80ffa7e5dc9eb88e40d46c48122281aea873
Abhishek Singh <ABHISHEK8801108@GMAIL.COM>
2026-01-24 19:34:39 +0530

    commit

--- EXPENSE_TRACKER/frontend/src/components/User/Dashboard/index.jsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = () => {
-    return (
-        <h1 className="text-5xl text-red-500 font-semibold">
-            Welcome to dashboard
-        </h1>
-    );
-};
-
-export default Dashboard;
--- EXPENSE_TRACKER/frontend/src/utills/http.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const http = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3030",
-  withCredentials: true,
-});
-
-export default http;
